acd0f844980f1065aac6d3150d829f35ef12c66b
nitinkanasnale <nitinkanasnale9353@gmail.com>
2025-04-09 09:44:53 +0530

    Updated ArrayReverse.java with new values and fixed logic

--- Day1_Arrays/ArrayReverse.java
+++ Day1_Arrays/ArrayReverse.java
@@ -4,18 +4,17 @@ public class ArrayReverse {
     public static void main(String[] args) {
         
     
-    int[] arr = {93, 53, 26, 31, 54};
-    System.out.println("Original array:");
-    for(int i = 0; i < arr.length; i++) {
+        int[] arr = {93, 53, 26, 31, 54};
+        System.out.println("Original array:");
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+
+        System.out.println("Reversed Array:");
+        for(int i = arr.length - 1; i >= 0; i-- ) {
         System.out.println(arr[i] + " ");
+        }
+
     }
-
-    System.out.println("Reversed Array:");
-    for(int i = arr.length - 1; i >= 0; i-- ) {
-    System.out.println(arr[i] + " ");
-    }
-
-}
-
-    
+   
 }
