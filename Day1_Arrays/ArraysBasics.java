e87ea83884310b939af42b3a4194525c2ef542eb
nitinkanasnale <nitinkanasnale9353@gmail.com>
2025-04-09 07:55:36 +0530

    Day 1: Added ArrayMaxMin.java and updated ArraysBasics.java

---
+++ Day1_Arrays/ArraysBasics.java
@@ -0,0 +1,12 @@
+package Day1_Arrays;
+
+public class ArraysBasics {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        System.out.println("Array elements:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
